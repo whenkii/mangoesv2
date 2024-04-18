7b2c979220d279c54d11f1d559c6fd3406ba03b2
whenkii <whenkii@yahoo.co.in>
2024-04-19 03:53:15 +0800

    reactconfig file

--- src/components/reactConfig.js
+++ src/components/reactConfig.js
@@ -1,8 +1,8 @@
 export const config = {
-    restAPIHost:"7001",restAPIserver:"http://localhost"
+    // restAPIHost:"7001",restAPIserver:"http://localhost"
     // restAPIHost:"7001",restAPIserver:"https://18.205.193.193"
     // restAPIHost:"7001",restAPIserver:"https://35.197.7.67"
-    // restAPIHost:"7001",restAPIserver:"https://www.garden-roots.com"
+    restAPIHost:"7001",restAPIserver:"https://www.garden-roots.com"
 }
 
 // console.log(process.platform)
